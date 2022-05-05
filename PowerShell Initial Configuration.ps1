ebcb004f6a16e099ea4dcc399398e0758ade1f33
AventureCloud <93791265+AventureCloud@users.noreply.github.com>
2022-05-05 05:47:59 -0400

    Create PowerShell Initial Configuration.ps1

---
+++ PowerShell Initial Configuration.ps1
@@ -0,0 +1,22 @@
+﻿#Lab- Initial PowerShell Configuration
+
+#Allow PowerShell to execute scripts
+Set-ExecutionPolicy Unrestricted -force
+
+
+#Configure PowerShell to use TLS 1.2 only
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+#Install Nuget package Manager
+Install-PackageProvider -Name NuGet -Force
+
+#Install PowerShellGet
+Install-Module -Name PowerShellGet -Force
+
+#Trusting the PowerShell Gallery
+
+#First verify the Repositories configured in your computer
+Get-PSRepository
+
+#Set the installation policy for a repository as trusted
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
